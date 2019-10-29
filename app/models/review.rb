b2dda509224b36d687bca37009fbd15e792eeb71
Kingsley Lau <lau.kingsley@gmail.com>
2019-10-29 16:40:48 +0800

    changed review wording and fixed review model validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   validates :name, presence: true
+  validates :email, presence: true
   validates :comment, presence: true
+  validates :stars, presence: true
 end
