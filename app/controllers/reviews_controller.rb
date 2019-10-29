4a1f439d9c24615733d6e6cc744dcde665dfa66c
Kingsley Lau <lau.kingsley@gmail.com>
2019-10-29 18:20:01 +0800

    added social media icons to nav bar

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
   def index
     @reviews = Review.all
+    @average = @reviews.sum(:stars).round(2) / @reviews.count
   end
 
   def new
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,9 @@ class Review < ApplicationRecord
   validates :email, presence: true
   validates :comment, presence: true
   validates :stars, presence: true
+
+  # def average_rating
+  #   average = reviews.sum(:stars).round(2)
+  # end
+
 end
