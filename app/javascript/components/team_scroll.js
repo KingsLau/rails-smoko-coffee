31bd579e85de63d3e32c5ce1f148731a7a0fcd45
Kingsley Lau <lau.kingsley@gmail.com>
2019-11-13 17:43:33 +0800

    minor styling changes to nav bar and homepage

--- app/javascript/components/team_scroll.js
+++ app/javascript/components/team_scroll.js
@@ -1,13 +1,27 @@
-var initialSrc = "/assets/founder-13e9c0be46a53c4859ae85334a2815bf7846ea7bfa6a3da4a2a86a2510e33977.jpg";
-var scrollSrc = "/assets/menu-e68d9ac20913e080e8af1ec562b3bf1628ab24a0c55e2d51da68cc7e170712ce.jpg";
+let initialSrc = "/assets/founder-13e9c0be46a53c4859ae85334a2815bf7846ea7bfa6a3da4a2a86a2510e33977.jpg";
+let scrollSrc = "/assets/menu-e68d9ac20913e080e8af1ec562b3bf1628ab24a0c55e2d51da68cc7e170712ce.jpg";
+// let element = document.getElementById("abc");
 
-$(window).scroll(function() {
-   var value = $(this).scrollTop();
-   if (value > 750)
-      $(".person").attr("src", scrollSrc);
-   else
+$(window ).scroll(function() {
+  // If user didn't scroll 500px set default z-index
+  if ($(this).scrollTop()>500) {
+    $(".person").attr("src", scrollSrc);
+    // element.classList.add("hidden");
+  } else {
+      // If user scrolled 1200px change logo's z-index to 9999
       $(".person").attr("src", initialSrc);
+      // element.classList.remove("hidden");
+  }
 });
+
+// $(window).scroll(function() {
+//    let value = $(this).scrollTop();
+//    // if (value > 750)
+//    if (value > 500)
+//       $(".person").attr("src", scrollSrc);
+//    else
+//       $(".person").attr("src", initialSrc);
+// });
 
 // <img class="person" src="/assets/founder-13e9c0be46a53c4859ae85334a2815bf7846ea7bfa6a3da4a2a86a2510e33977.jpg">
 // <img class="person" src="/assets/founder-3e9c0be46a53c4859ae85334a2815bf7846ea7bfa6a3da4a2a86a2510e33977.jpg">
